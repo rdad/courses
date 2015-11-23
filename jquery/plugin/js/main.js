8e6e7bba9f17a0689864f8ec47789c8549aed043
rdad <akindofceasar@gmail.com>
2015-11-23 11:35:43 +0100

    jquery vegas exemple added

---
+++ jquery/plugin/js/main.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+
+	$("body").vegas({
+	    slides: [
+	        { src: "/img/img1.jpg" },
+	        { src: "/img/img2.jpg" },
+	        { src: "/img/img3.jpg" },
+	        { src: "/img/img4.jpg" },
+	        { src: "/img/img5.jpg" },
+	        { src: "/img/img6.jpg" }
+	    ],
+	    overlay: 'js/vegas/overlays/07.png',
+	    transition: 'zoomIn'
+	});
+
+	
+
+});
